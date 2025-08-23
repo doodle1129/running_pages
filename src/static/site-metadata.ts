ca5bff099bf35afd9e19dd095be0d8d9fa470468
doodle1129 <doodlezz@126.com>
2025-08-23 20:57:55 +0800

    firstcommit

---
+++ src/static/site-metadata.ts
@@ -0,0 +1,38 @@
+interface ISiteMetadataResult {
+  siteTitle: string;
+  siteUrl: string;
+  description: string;
+  logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const getBasePath = () => {
+  const baseUrl = import.meta.env.BASE_URL;
+  return baseUrl === '/' ? '' : baseUrl;
+};
+
+const data: ISiteMetadataResult = {
+  siteTitle: 'Running Page',
+  siteUrl: 'https://doodle1129.github.io/running_pages/',
+  logo: 'https://avatars.githubusercontent.com/u/189682565?s=400&u=ea676e2a2dd59ee5a1f28ea5d9a1900ddc3c3727&v=4',
+  description: 'Personal site and blog',
+  navLinks: [
+    {
+      name: 'Summary',
+      url: `${getBasePath()}/summary`,
+    },
+    {
+      name: 'Workouts Page',
+      url: 'https://doodle1129.github.io/workouts_page/',
+    },
+    {
+      name: 'About',
+      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+    },
+  ],
+};
+
+export default data;
